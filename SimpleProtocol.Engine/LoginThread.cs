ad19f45eec3f620852ea2b01b67d99572d1a1407
PajoPajo <pajo@atlas.cz>
2018-02-08 09:10:39 +0100

    LoginThreadCurrentPrincipal : ILogin

---
+++ SimpleProtocol.Engine/LoginThread.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using SimpleProtocol.Contract;
+
+namespace SimpleProtocol.Engine
+{
+    public class LoginThreadCurrentPrincipal : ILogin
+    {
+        public string Login => Thread.CurrentPrincipal?.Identity?.Name;
+    }
+}
